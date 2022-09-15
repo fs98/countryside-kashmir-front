901335c63f4d916169d926bfa82f9acdf3f9c61a
Fata Sefer <sefer.fata@gmail.com>
2022-09-15 19:02:48 +0200

    ui: create Block component

---
+++ src/components/Block/Block.tsx
@@ -0,0 +1,35 @@
+import { Container, Typography } from '@mui/material';
+import { FC, PropsWithChildren } from 'react';
+
+type BlockProps = PropsWithChildren & {
+  title: string;
+  subtitle?: string;
+};
+
+const Block: FC<BlockProps> = ({ title, subtitle, children }) => (
+  <Container maxWidth="lg" sx={{ py: 6 }}>
+    <Typography
+      variant="h4"
+      color="warning.main"
+      fontWeight="bold"
+      textAlign="center"
+      textTransform="uppercase">
+      {title}
+    </Typography>
+
+    {subtitle && (
+      <Typography
+        variant="h6"
+        color="text.primary"
+        fontWeight="light"
+        textAlign="center"
+        marginTop={2}>
+        {subtitle}
+      </Typography>
+    )}
+
+    {children}
+  </Container>
+);
+
+export default Block;
