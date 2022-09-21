2af13c5a6daff8c096e5b7c64fca8d3a0fdddb01
Fata Sefer <sefer.fata@gmail.com>
2022-09-21 17:45:28 +0200

    feat: display blogs in blogs.tsx

--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -1,12 +1,22 @@
 import Block from '@/components/Block/Block';
+import SimpleCard from '@/components/Cards/SimpleCard/SimpleCard';
 import ImageHeaderLayout from '@/layouts/ImageHeaderLayout';
 import axios from '@/lib/axios';
+import { Grid } from '@mui/material';
 import { FC } from 'react';
 import { BlogProps } from '.';
 
 export const Blogs: FC<{ blogs: BlogProps[] }> = ({ blogs }) => (
   <ImageHeaderLayout title="Blogs - Countryside Kashmir" heading="Blogs">
-    <Block title="Blogs"></Block>
+    <Block title="Blogs">
+      <Grid marginTop={6} container rowSpacing={5} spacing={5} justifyContent="center">
+        {blogs.map((blog, i) => (
+          <Grid key={i} item xs={6} md={4} lg={3}>
+            <SimpleCard item={blog} />
+          </Grid>
+        ))}
+      </Grid>
+    </Block>
   </ImageHeaderLayout>
 );
 
@@ -14,7 +24,7 @@ export const getServerSideProps = async () => {
   const blogs = await axios
     .get('/api/guest/blogs')
     .then(res => {
-      return res.data;
+      return res.data.data;
     })
     .catch(error => {
       if (error.response.status !== 409) throw error;
