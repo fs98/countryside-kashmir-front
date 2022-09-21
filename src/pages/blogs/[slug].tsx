5b463b05b5eb0b36bc1cd289b55cbda65496c971
Fata Sefer <sefer.fata@gmail.com>
2022-09-21 19:08:07 +0200

    feat: create blog subpage

---
+++ src/pages/blogs/[slug].tsx
@@ -0,0 +1,41 @@
+import Block from '@/components/Block/Block';
+import ImageHeaderLayout from '@/layouts/ImageHeaderLayout';
+import axios from '@/lib/axios';
+import { redirect } from 'next/dist/server/api-utils';
+import { FC } from 'react';
+import { BlogsProps } from '..';
+
+type BlogProps = {
+  blog: BlogsProps & {
+    keywords: string;
+    published_at: string;
+  };
+};
+
+const Blog: FC<BlogProps> = ({ blog }) => {
+  return (
+    <ImageHeaderLayout title={blog.title} heading="Blog">
+      <Block title={blog.title} subtitle={blog.published_at}></Block>
+    </ImageHeaderLayout>
+  );
+};
+
+export const getServerSideProps = async ({ params: { slug } }) => {
+  const blog = await axios
+    .get(`/api/guest/blogs/${slug}`)
+    .then(res => {
+      console.log(res.data.data);
+      return res.data.data;
+    })
+    .catch(error => {
+      if (error.response.status !== 409) throw error;
+    });
+
+  return {
+    props: {
+      blog,
+    },
+  };
+};
+
+export default Blog;
