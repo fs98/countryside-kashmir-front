99b7bbcb8886499a5a7a8163a3ad5cf223e7bf43
Fata Sefer <sefer.fata@gmail.com>
2022-09-22 14:36:30 +0200

    feat: render editorjs block in blogs

--- src/pages/blogs/[slug].tsx
+++ src/pages/blogs/[slug].tsx
@@ -1,5 +1,6 @@
 import { Block } from '@/components/Block/Block';
 import { ImageHeaderLayout } from '@/layouts/ImageHeaderLayout';
+import Blocks from 'editorjs-blocks-react-renderer';
 import { axios } from '@/lib/axios';
 import moment from 'moment';
 import { FC } from 'react';
@@ -18,7 +19,49 @@ const Blog: FC<BlogProps> = ({ blog }) => {
   return (
     <ImageHeaderLayout title={blog.title} heading="Blog">
       <Block title={blog.title} subtitle={published_at}>
-        {JSON.stringify(blog.content.blocks)}
+        <Blocks
+          data={blog.content}
+          config={{
+            code: {
+              className: 'language-js',
+            },
+            delimiter: {
+              className: 'border border-2 w-16 mx-auto',
+            },
+            embed: {
+              className: 'border-0',
+            },
+            header: {
+              className: 'font-bold',
+            },
+            image: {
+              className: 'w-full max-w-screen-md',
+              actionsClassNames: {
+                stretched: 'w-full h-80 object-cover',
+                withBorder: 'border border-2',
+                withBackground: 'p-2',
+              },
+            },
+            list: {
+              className: 'list-inside',
+              type: {
+                unordered: 'list-disc',
+              },
+            },
+            paragraph: {
+              className: 'text-base text-opacity-75 mb-5',
+              actionsClassNames: {
+                alignment: 'text-{alignment}', // This is a substitution placeholder: left or center.
+              },
+            },
+            quote: {
+              className: 'py-3 px-5 italic font-serif',
+            },
+            table: {
+              className: 'table-auto',
+            },
+          }}
+        />
       </Block>
     </ImageHeaderLayout>
   );
