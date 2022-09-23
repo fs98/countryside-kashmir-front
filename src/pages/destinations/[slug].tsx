7febe16fefc9f80bd845c196521f2d94382de6d7
Fata Sefer <sefer.fata@gmail.com>
2022-09-24 00:23:38 +0200

    feat: render content in destinations/[slug] page

--- src/pages/destinations/[slug].tsx
+++ src/pages/destinations/[slug].tsx
@@ -1,6 +1,71 @@
+import React from 'react';
+
+import { Block } from '@/components/Block/Block';
+import { ImageHeaderLayout } from '@/layouts/ImageHeaderLayout';
+
+import Blocks from 'editorjs-blocks-react-renderer';
+
+import { ContentProps, ImageProps, ItemsProps } from '..';
+
 import { axios } from '@/lib/axios';
 
-const Destination = () => <div>Destination</div>;
+type DestinationProps = {
+  destination: ItemsProps & {
+    description: ContentProps;
+    keywords: string;
+    destination_images: ImageProps[];
+  };
+};
+
+const Destination = ({ destination }: DestinationProps) => (
+  <ImageHeaderLayout title={destination.name} heading="Blog">
+    <Block title={destination.name} subtitle="subtitle">
+      <Blocks
+        data={destination.description}
+        config={{
+          code: {
+            className: 'language-js',
+          },
+          delimiter: {
+            className: 'border border-2 w-16 mx-auto',
+          },
+          embed: {
+            className: 'border-0',
+          },
+          header: {
+            className: 'font-bold',
+          },
+          image: {
+            className: 'w-full max-w-screen-md',
+            actionsClassNames: {
+              stretched: 'w-full h-80 object-cover',
+              withBorder: 'border border-2',
+              withBackground: 'p-2',
+            },
+          },
+          list: {
+            className: 'list-inside',
+            type: {
+              unordered: 'list-disc',
+            },
+          },
+          paragraph: {
+            className: 'text-base text-opacity-75 mb-5',
+            actionsClassNames: {
+              alignment: 'text-{alignment}', // This is a substitution placeholder: left or center.
+            },
+          },
+          quote: {
+            className: 'py-3 px-5 italic font-serif',
+          },
+          table: {
+            className: 'table-auto',
+          },
+        }}
+      />
+    </Block>
+  </ImageHeaderLayout>
+);
 
 export const getServerSideProps = async ({ params: { slug } }) => {
   const destination = await axios
