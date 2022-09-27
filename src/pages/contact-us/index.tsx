2af0a883d98715e31b73364d871ce577bcbed00e
Fata Sefer <sefer.fata@gmail.com>
2022-09-27 22:52:25 +0200

    feat: reposition error messages in contact us page

--- src/pages/contact-us/index.tsx
+++ src/pages/contact-us/index.tsx
@@ -1,5 +1,6 @@
 import {
   Alert,
+  Box,
   Button,
   Fade,
   FormControl,
@@ -7,8 +8,15 @@ import {
   Grid,
   Input,
   InputLabel,
+  Link,
+  Stack,
+  Typography,
 } from '@mui/material';
 import { SyntheticEvent, useRef, useState } from 'react';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import MailIcon from '@mui/icons-material/Mail';
+import PhoneEnabledIcon from '@mui/icons-material/PhoneEnabled';
+import PeopleIcon from '@mui/icons-material/People';
 import { Block } from '@/components/Block/Block';
 import { ImageHeaderLayout } from '@/layouts/ImageHeaderLayout';
 import { axios } from '@/lib/axios';
@@ -48,23 +56,31 @@ export const ContactUs = () => {
       .catch(error => {
         const errors = [];
 
-        // Get first_name errors
-        if (error.response.data.errors.first_name) {
-          error.response.data.errors.first_name.map((error: string) => {
-            errors.push(error);
+        if (error.response.status === 500) {
+          setMessage({
+            title: error.response.data.message,
+            type: 'error',
           });
         }
 
-        if (error.response.data.errors.phone_number) {
-          error.response.data.errors.phone_number.map((error: string) => {
-            errors.push(error);
+        if (error.response.status === 422) {
+          if (error.response.data.errors.first_name) {
+            error.response.data.errors.first_name.map((error: string) => {
+              errors.push(error);
+            });
+          }
+
+          if (error.response.data.errors.phone_number) {
+            error.response.data.errors.phone_number.map((error: string) => {
+              errors.push(error);
+            });
+          }
+
+          setMessage({
+            title: errors.join(' '),
+            type: 'error',
           });
         }
-
-        setMessage({
-          title: errors.join(' '),
-          type: 'error',
-        });
 
         setTimeout(() => {
           setMessage(undefined);
@@ -81,17 +97,82 @@ export const ContactUs = () => {
 
   return (
     <ImageHeaderLayout title="Contact us - Countryside Kashmir" heading="Contact us">
-      <Block title="Contact Us">
-        {message && (
-          <Fade in={true}>
-            <Alert onClose={() => setMessage(undefined)} color={message.type}>
-              {message.title}
-            </Alert>
-          </Fade>
-        )}
+      <Block>
         <Grid container spacing={2} sx={{ marginTop: 4 }}>
           {/* Left Side */}
-          <Grid item md={6}>
+          <Grid item sm={12} md={6}>
+            <Typography variant="h5" fontWeight="bold" marginBottom={4}>
+              Please Feel Free To Contact Us
+            </Typography>
+            <Stack marginBottom={4}>
+              <Typography variant="h6" fontWeight="bold" color="warning.main">
+                <LocationOnIcon />
+                Head Office:
+              </Typography>
+              <Typography variant="body1">Gulmarg Road, Chandilora Tangmarg Baramulla</Typography>
+              <Typography variant="body1">193402 Jammu and Kashmir - India</Typography>
+            </Stack>
+            <Stack marginBottom={4}>
+              <Typography variant="h6" fontWeight="bold" color="warning.main">
+                <LocationOnIcon />
+                Branch Office:
+              </Typography>
+              <Typography variant="body1">
+                33, Kazi Nazrul Islam Avenue, Hafiz Menstion (3rd Floor) kawran Bazar
+              </Typography>
+              <Typography variant="body1">Dhaka, Bangladesh</Typography>
+              <Typography variant="body1">Contact : +8801700767245</Typography>
+            </Stack>
+            <Stack marginBottom={4}>
+              <Typography variant="h6" fontWeight="bold" color="warning.main">
+                <MailIcon />
+                Email:
+              </Typography>
+              <Typography variant="body1">countrysidekashmir@gmail.com</Typography>
+            </Stack>
+            <Stack marginBottom={4}>
+              <Typography variant="h6" fontWeight="bold" color="warning.main">
+                <PhoneEnabledIcon />
+                Phone:
+              </Typography>
+              <Typography variant="body1">+91-9596404872</Typography>
+              <Typography variant="body1">+91-9596404875</Typography>
+              <Typography variant="body1">+91-9419555772</Typography>
+            </Stack>
+            <Stack marginBottom={4}>
+              <Typography variant="h6" fontWeight="bold" color="warning.main">
+                <PeopleIcon />
+                Social Media:
+              </Typography>
+              <Box display="flex" alignItems="center" marginTop={1}>
+                <Link href="https://www.tripadvisor.in/Attraction_Review-g297623-d15557712-Reviews-Countryside_Kashmir_Tour_Travel-Srinagar_Srinagar_District_Kashmir_Jammu_and_Kas.html">
+                  <img width={30} src="https://countrysidekashmir.com/img/tripadvisor.png" alt="" />
+                </Link>
+                <Link marginLeft={1} href="https://www.fLinkcebook.com/countrysidekashmir">
+                  <img width={30} src="https://countrysidekashmir.com/img/facebook.png" alt="" />
+                </Link>
+                <Link marginLeft={1} href="https://www.instagram.com/countrysidekashmir/">
+                  <img width={30} src="https://countrysidekashmir.com/img/instagram.png" alt="" />
+                </Link>
+                <Link
+                  marginLeft={1}
+                  href="https://www.google.com/maps/place/Countryside+Kashmir+Tour+%26+Travel/@34.0690528,74.4500511,15z/data=!4m5!3m4!1s0x0:0x8ce50dbaaad9ca86!8m2!3d34.0690528!4d74.4500511?shorturl=1">
+                  <img width={30} src="https://countrysidekashmir.com/img/google_map.png" alt="" />
+                </Link>
+                <Link
+                  marginLeft={1}
+                  href="https://www.youtube.com/channel/UCxe23fscAkpQ2TOsnnKtkpQ">
+                  <img width={30} src="https://countrysidekashmir.com/img/youtube.png" alt="" />
+                </Link>
+              </Box>
+            </Stack>
+          </Grid>
+
+          {/* Left Side */}
+          <Grid item sm={12} md={6}>
+            <Typography variant="h5" fontWeight="bold" marginBottom={4}>
+              Or Fill Out Our Contact Form
+            </Typography>
             {/* Contact Form */}
             <form onSubmit={submitHandler}>
               <FormGroup row={true}>
@@ -124,11 +205,16 @@ export const ContactUs = () => {
                 Submit
               </Button>
             </form>
-          </Grid>
-
-          {/* Right Side */}
-          <Grid item md={6}>
-            second side
+            {message && (
+              <Fade in={true}>
+                <Alert
+                  sx={{ marginTop: 2 }}
+                  onClose={() => setMessage(undefined)}
+                  color={message.type}>
+                  {message.title}
+                </Alert>
+              </Fade>
+            )}
           </Grid>
         </Grid>
       </Block>
