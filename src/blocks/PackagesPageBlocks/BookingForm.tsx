58ac11133edda461753c3c11e9b7a2e30308607a
Fata Sefer <sefer.fata@gmail.com>
2022-09-28 23:26:48 +0200

    refactor: optimize form fields generation

--- src/blocks/PackagesPageBlocks/BookingForm.tsx
+++ src/blocks/PackagesPageBlocks/BookingForm.tsx
@@ -1,6 +1,16 @@
-import { Alert, Button, Fade, FormControl, FormHelperText, Input, InputLabel } from '@mui/material';
+import {
+  Alert,
+  Button,
+  Fade,
+  FormControl,
+  FormHelperText,
+  Input,
+  InputLabel,
+  Stack,
+} from '@mui/material';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import _ from 'lodash';
 import { axios } from '@/lib/axios';
 
 type FormData = {
@@ -17,6 +27,21 @@ type FormData = {
   days: number;
   nights: number;
 };
+
+type FormFieldRulesProps = {
+  name: string;
+  value: unknown;
+  text: string;
+};
+
+type FormFieldProps = {
+  id: keyof FormData;
+  label: string;
+  type: 'text' | 'number' | 'date' | 'email';
+  rules: FormFieldRulesProps[];
+};
+
+type BookingFormFieldProps = FormFieldProps[];
 
 const BookingForm = () => {
   const {
@@ -80,222 +105,244 @@ const BookingForm = () => {
           setTimeout(() => {
             setMessage(undefined);
           }, 3000);
-        })
-        .finally(() => {});
+        });
     },
   );
 
   const [message, setMessage] = useState<{ title: string; type: 'success' | 'error' }>();
 
+  const bookingFormFields: BookingFormFieldProps = [
+    {
+      id: 'name',
+      label: 'Name',
+      type: 'text',
+      rules: [
+        {
+          name: 'required',
+          value: true,
+          text: 'Name is required.',
+        },
+        {
+          name: 'maxLength',
+          value: 64,
+          text: 'Name should be shorther than 64 characters.',
+        },
+      ],
+    },
+    {
+      id: 'email',
+      label: 'Email',
+      type: 'email',
+      rules: [
+        {
+          name: 'maxLength',
+          value: 64,
+          text: 'Email should be shorther than 64 characters.',
+        },
+      ],
+    },
+    {
+      id: 'phoneNumber',
+      label: 'Phone number',
+      type: 'text',
+      rules: [
+        {
+          name: 'required',
+          value: true,
+          text: 'Phone number is required.',
+        },
+        {
+          name: 'maxLength',
+          value: 128,
+          text: 'Phone number should be shorther than 128 characters.',
+        },
+      ],
+    },
+    {
+      id: 'address',
+      label: 'Address',
+      type: 'text',
+      rules: [
+        {
+          name: 'required',
+          value: true,
+          text: 'Address is required.',
+        },
+        {
+          name: 'maxLength',
+          value: 128,
+          text: 'Address should be shorther than 128 characters.',
+        },
+      ],
+    },
+    {
+      id: 'city',
+      label: 'City',
+      type: 'text',
+      rules: [
+        {
+          name: 'required',
+          value: true,
+          text: 'City is required.',
+        },
+        {
+          name: 'maxLength',
+          value: 128,
+          text: 'City should be shorther than 128 characters.',
+        },
+      ],
+    },
+    {
+      id: 'country',
+      label: 'Country',
+      type: 'text',
+      rules: [
+        {
+          name: 'required',
+          value: true,
+          text: 'Country is required.',
+        },
+        {
+          name: 'maxLength',
+          value: 128,
+          text: 'Country should be shorther than 128 characters.',
+        },
+      ],
+    },
+    {
+      id: 'persons',
+      label: 'Persons',
+      type: 'number',
+      rules: [
+        {
+          name: 'required',
+          value: true,
+          text: 'Number of persons is required.',
+        },
+        {
+          name: 'min',
+          value: 1,
+          text: 'Number of persons should be greater than 1.',
+        },
+      ],
+    },
+    {
+      id: 'adults',
+      label: 'Adults',
+      type: 'number',
+      rules: [
+        {
+          name: 'required',
+          value: true,
+          text: 'Number of adults is required.',
+        },
+        {
+          name: 'min',
+          value: 1,
+          text: 'Number of adults should be greater than 1.',
+        },
+      ],
+    },
+    {
+      id: 'children',
+      label: 'Children',
+      type: 'number',
+      rules: [
+        {
+          name: 'required',
+          value: true,
+          text: 'Number of children is required.',
+        },
+        {
+          name: 'min',
+          value: 1,
+          text: 'Number of children should be greater than 1.',
+        },
+      ],
+    },
+    {
+      id: 'arrivalDate',
+      label: 'Arrival date',
+      type: 'date',
+      rules: [
+        {
+          name: 'required',
+          value: true,
+          text: 'Arrival date is required.',
+        },
+      ],
+    },
+    {
+      id: 'days',
+      label: 'Days',
+      type: 'number',
+      rules: [
+        {
+          name: 'required',
+          value: true,
+          text: 'Number of days is required.',
+        },
+        {
+          name: 'min',
+          value: 1,
+          text: 'Number of days should be greater than 1.',
+        },
+      ],
+    },
+    {
+      id: 'nights',
+      label: 'Nights',
+      type: 'number',
+      rules: [
+        {
+          name: 'required',
+          value: true,
+          text: 'Number of nights is required.',
+        },
+        {
+          name: 'min',
+          value: 1,
+          text: 'Number of nights should be greater than 1.',
+        },
+      ],
+    },
+  ];
+
   return (
     <>
       <form onSubmit={onSubmit}>
-        <FormControl fullWidth={true} color="warning" sx={{ marginTop: 4 }}>
-          <InputLabel htmlFor="name">Name</InputLabel>
+        <Stack spacing={2}>
+          {bookingFormFields.map(field => {
+            const errorHelperText =
+              errors?.[field.id]?.type &&
+              field.rules.find(err => err.name === errors[field.id].type);
 
-          <Input {...register('name', { required: true, maxLength: 64 })} id="name" />
+            return (
+              <FormControl key={field.id} fullWidth={true} color="warning">
+                <InputLabel htmlFor={field.id}>{field.label}</InputLabel>
 
-          {errors.name?.type === 'required' && (
-            <FormHelperText error id="component-error-text">
-              Name is required.
-            </FormHelperText>
-          )}
-          {errors.name?.type === 'maxLength' && (
-            <FormHelperText error id="component-error-text">
-              Name should be shorther than 64 characters.
-            </FormHelperText>
-          )}
-        </FormControl>
+                <Input
+                  {...register(
+                    field.id,
+                    _.fromPairs(field.rules.map(rule => [rule.name, rule.value])),
+                  )}
+                  id={field.id}
+                  type={field.type}
+                />
 
-        <FormControl fullWidth={true} color="warning" sx={{ marginTop: 4 }}>
-          <InputLabel htmlFor="email">Email</InputLabel>
-          <Input
-            {...register('email', { required: false, maxLength: 128 })}
-            type="email"
-            id="email"
-          />
-          {errors.email?.type === 'maxLength' && (
-            <FormHelperText error id="component-error-text">
-              Email should be shorther than 64 characters.
-            </FormHelperText>
-          )}
-        </FormControl>
+                {errorHelperText && (
+                  <FormHelperText error id="component-error-text">
+                    {errorHelperText.text}
+                  </FormHelperText>
+                )}
+              </FormControl>
+            );
+          })}
 
-        <FormControl fullWidth={true} color="warning" sx={{ marginTop: 4 }}>
-          <InputLabel htmlFor="phone-number">Phone number</InputLabel>
-
-          <Input
-            {...register('phoneNumber', { required: true, maxLength: 128 })}
-            id="phone-number"
-          />
-
-          {errors.phoneNumber?.type === 'required' && (
-            <FormHelperText error id="component-error-text">
-              Phone number is required.
-            </FormHelperText>
-          )}
-          {errors.phoneNumber?.type === 'maxLength' && (
-            <FormHelperText error id="component-error-text">
-              Phone number should be shorther than 128 characters.
-            </FormHelperText>
-          )}
-        </FormControl>
-
-        <FormControl fullWidth={true} color="warning" sx={{ marginTop: 4 }}>
-          <InputLabel htmlFor="address">Adress</InputLabel>
-
-          <Input {...register('address', { required: true, maxLength: 128 })} id="address" />
-
-          {errors.address?.type === 'required' && (
-            <FormHelperText error id="component-error-text">
-              Address is required.
-            </FormHelperText>
-          )}
-          {errors.address?.type === 'maxLength' && (
-            <FormHelperText error id="component-error-text">
-              Address should be shorther than 128 characters.
-            </FormHelperText>
-          )}
-        </FormControl>
-
-        <FormControl fullWidth={true} color="warning" sx={{ marginTop: 4 }}>
-          <InputLabel htmlFor="city">City</InputLabel>
-
-          <Input {...register('city', { required: true, maxLength: 128 })} id="city" />
-
-          {errors.city?.type === 'required' && (
-            <FormHelperText error id="component-error-text">
-              City is required.
-            </FormHelperText>
-          )}
-          {errors.city?.type === 'maxLength' && (
-            <FormHelperText error id="component-error-text">
-              City should be shorther than 128 characters.
-            </FormHelperText>
-          )}
-        </FormControl>
-
-        <FormControl fullWidth={true} color="warning" sx={{ marginTop: 4 }}>
-          <InputLabel htmlFor="country">Country</InputLabel>
-
-          <Input {...register('country', { required: true, maxLength: 128 })} id="country" />
-
-          {errors.country?.type === 'required' && (
-            <FormHelperText error id="component-error-text">
-              Country is required.
-            </FormHelperText>
-          )}
-          {errors.country?.type === 'maxLength' && (
-            <FormHelperText error id="component-error-text">
-              Country should be shorther than 128 characters.
-            </FormHelperText>
-          )}
-        </FormControl>
-
-        <FormControl fullWidth={true} color="warning" sx={{ marginTop: 4 }}>
-          <InputLabel htmlFor="persons">Persons</InputLabel>
-
-          <Input {...register('persons', { required: true, min: 1 })} type="number" id="persons" />
-
-          {errors.persons?.type === 'required' && (
-            <FormHelperText error id="component-error-text">
-              Number of persons is required.
-            </FormHelperText>
-          )}
-          {errors.persons?.type === 'min' && (
-            <FormHelperText error id="component-error-text">
-              Number of persons should be greater than 1.
-            </FormHelperText>
-          )}
-        </FormControl>
-
-        <FormControl fullWidth={true} color="warning" sx={{ marginTop: 4 }}>
-          <InputLabel htmlFor="adults">Adults</InputLabel>
-
-          <Input {...register('adults', { required: true, min: 1 })} type="number" id="adults" />
-
-          {errors.adults?.type === 'required' && (
-            <FormHelperText error id="component-error-text">
-              Number of adults is required.
-            </FormHelperText>
-          )}
-          {errors.adults?.type === 'min' && (
-            <FormHelperText error id="component-error-text">
-              Number of adults should be greater than 1.
-            </FormHelperText>
-          )}
-        </FormControl>
-
-        <FormControl fullWidth={true} color="warning" sx={{ marginTop: 4 }}>
-          <InputLabel htmlFor="children">Children</InputLabel>
-
-          <Input
-            {...register('children', { required: true, min: 1 })}
-            type="number"
-            id="children"
-          />
-
-          {errors.children?.type === 'required' && (
-            <FormHelperText error id="component-error-text">
-              Number of adults is required.
-            </FormHelperText>
-          )}
-          {errors.children?.type === 'min' && (
-            <FormHelperText error id="component-error-text">
-              Number of adults should be greater than 1.
-            </FormHelperText>
-          )}
-        </FormControl>
-
-        <FormControl fullWidth={true} color="warning" sx={{ marginTop: 4 }}>
-          <InputLabel htmlFor="arrival-date">Arrival date</InputLabel>
-
-          <Input {...register('arrivalDate', { required: true })} type="date" id="arrival-date" />
-
-          {errors.arrivalDate?.type === 'required' && (
-            <FormHelperText error id="component-error-text">
-              Arrival date is required.
-            </FormHelperText>
-          )}
-        </FormControl>
-
-        <FormControl fullWidth={true} color="warning" sx={{ marginTop: 4 }}>
-          <InputLabel htmlFor="days">Days</InputLabel>
-
-          <Input {...register('days', { required: true, min: 1 })} type="number" id="days" />
-
-          {errors.days?.type === 'required' && (
-            <FormHelperText error id="component-error-text">
-              Number of adults is required.
-            </FormHelperText>
-          )}
-          {errors.days?.type === 'min' && (
-            <FormHelperText error id="component-error-text">
-              Number of adults should be greater than 1.
-            </FormHelperText>
-          )}
-        </FormControl>
-
-        <FormControl fullWidth={true} color="warning" sx={{ marginTop: 4 }}>
-          <InputLabel htmlFor="nights">Nights</InputLabel>
-
-          <Input {...register('nights', { required: true, min: 1 })} type="number" id="nights" />
-
-          {errors.nights?.type === 'required' && (
-            <FormHelperText error id="component-error-text">
-              Number of nights is required.
-            </FormHelperText>
-          )}
-          {errors.nights?.type === 'min' && (
-            <FormHelperText error id="component-error-text">
-              Number of nights should be greater than 1.
-            </FormHelperText>
-          )}
-        </FormControl>
-
-        <Button variant="outlined" color="warning" type="submit" sx={{ marginTop: 4 }}>
-          Submit
-        </Button>
+          <Button variant="outlined" color="warning" type="submit">
+            Submit
+          </Button>
+        </Stack>
       </form>
       {message && (
         <Fade in={true}>
