b37091f3d5b5be20a1d3edb328caa0037bebf4be
Fata Sefer <sefer.fata@gmail.com>
2022-09-28 23:33:03 +0200

    chore: install @typescript-eslint/parser and set react version in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,4 +44,11 @@ module.exports = {
   globals: {
     JSX: true,
   },
+  settings: {
+    react: {
+      version: 'detect', // React version. "detect" automatically picks the version you have installed.
+      // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+      // It will default to "latest" and warn if missing, and to "detect" in the future
+    },
+  },
 };
