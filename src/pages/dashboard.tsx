5e7f7f6b3662446e13f73d431001cb3181ddc859
Fata Sefer <sefer.fata@gmail.com>
2022-09-30 23:11:04 +0200

    feat: retrieve messages in getServerSideProps

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,14 +1,18 @@
 import Head from 'next/head';
 import { AppLayout } from '@/layouts/AppLayout';
+import { axios } from '@/lib/axios';
 
-const Dashboard = () => {
+const Dashboard = ({ messages }) => {
   return (
     <AppLayout
       header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}>
       <Head>
-        <title>Laravel - Dashboard</title>
+        <title>Countryside Kashmir - Dashboard</title>
       </Head>
 
+      {messages.map(message => (
+        <div key={message.id}>{message.id}</div>
+      ))}
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -18,6 +22,32 @@ const Dashboard = () => {
       </div>
     </AppLayout>
   );
+};
+
+export const getServerSideProps = async ({
+  req: {
+    headers: { cookie, host },
+  },
+}) => {
+  const messages = await axios
+    .get('/api/messages', {
+      headers: {
+        Cookie: cookie,
+        Referer: host,
+      },
+    })
+    .then(res => {
+      return res.data.data;
+    })
+    .catch(error => {
+      if (error.response.status !== 409) throw error;
+    });
+
+  return {
+    props: {
+      messages,
+    },
+  };
 };
 
 export default Dashboard;
