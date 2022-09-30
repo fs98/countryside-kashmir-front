c1a4718cf76d22b773b4e358be9274764a59d582
Fata Sefer <sefer.fata@gmail.com>
2022-09-30 23:29:18 +0200

    refactor: remove console.log

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -17,7 +17,6 @@ const columns: GridColDef[] = [
 ];
 
 const Dashboard = ({ messages }) => {
-  console.log(messages);
   return (
     <AppLayout
       header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}>
