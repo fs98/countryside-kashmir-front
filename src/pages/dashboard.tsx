94d72a39ecbc936e0e439fff628dda8ab2cb52a5
Fata Sefer <sefer.fata@gmail.com>
2022-10-01 22:51:32 +0200

    style: change table height in dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -19,14 +19,16 @@ const columns: GridColDef[] = [
 const Dashboard = ({ messages }) => {
   return (
     <AppLayout
-      header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}>
+      header={
+        <h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard - messages</h2>
+      }>
       <Head>
         <title>Countryside Kashmir - Dashboard</title>
       </Head>
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div className="p-6 bg-white border-b border-gray-200 w-full" style={{ height: 700 }}>
+            <div className="p-6 bg-white border-b border-gray-200 w-full h-96">
               <DataGrid rows={messages} columns={columns} />
             </div>
           </div>
