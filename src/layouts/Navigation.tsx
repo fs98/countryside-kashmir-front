d25ca098aaebd77cdaade54bd61c53d5f33c0ef5
Fata Sefer <sefer.fata@gmail.com>
2022-10-02 22:31:25 +0200

    refactor: replace wrong links

--- src/layouts/Navigation.tsx
+++ src/layouts/Navigation.tsx
@@ -33,7 +33,7 @@ export const Navigation: FC<NavigationProps> = ({ user }) => {
           <div className="flex">
             {/* Logo */}
             <div className="flex-shrink-0 flex items-center">
-              <Link href="/dashboard">
+              <Link href="/admin/dashboard">
                 <a>
                   <ApplicationLogo className="block h-10 w-auto fill-current text-gray-600" />
                 </a>
@@ -42,7 +42,7 @@ export const Navigation: FC<NavigationProps> = ({ user }) => {
 
             {/* Navigation Links */}
             <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-              <NavLink href="/dashboard" active={router.pathname === '/dashboard'}>
+              <NavLink href="/admin/dashboard" active={router.pathname === '/admin/dashboard'}>
                 Dashboard
               </NavLink>
               <NavLink href="/admin/categories" active={router.pathname === '/admin/categories'}>
@@ -112,7 +112,9 @@ export const Navigation: FC<NavigationProps> = ({ user }) => {
       {open && (
         <div className="block sm:hidden">
           <div className="pt-2 pb-3 space-y-1">
-            <ResponsiveNavLink href="/dashboard" active={router.pathname === '/dashboard'}>
+            <ResponsiveNavLink
+              href="/admin/dashboard"
+              active={router.pathname === '/admin/dashboard'}>
               Dashboard
             </ResponsiveNavLink>
             <ResponsiveNavLink
