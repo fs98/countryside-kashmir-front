7a78a41cc424eefebe3f249b909ce68dbe52c3b9
Fata Sefer <sefer.fata@gmail.com>
2022-10-05 00:11:17 +0200

    feat: display fetched slides as cards

--- src/pages/admin/slides.tsx
+++ src/pages/admin/slides.tsx
@@ -20,8 +20,27 @@ const Slides = ({ slides }: { slides: adminSlidesProps[] }): JSX.Element => {
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div className="p-6 bg-white border-b border-gray-200 w-full h-96">
-              {JSON.stringify(slides)}
+            <div className="p-6 bg-white border-b border-gray-200 w-full grid grid-cols-3 gap-4">
+              {slides.map(slide => (
+                <div key={slide.id} className="max-w-sm rounded overflow-hidden shadow-lg">
+                  <img className="w-full" src={slide.image_url} alt={slide.image_alt} />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">{slide.title}</div>
+                    <p className="text-gray-700 text-base">{slide.subtitle}</p>
+                  </div>
+                  <div className="px-6 pt-4 pb-2">
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                      #photography
+                    </span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                      #travel
+                    </span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                      #winter
+                    </span>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
