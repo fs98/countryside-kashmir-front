2071b3d33505a26b3aaf216448e42a6ce8de72d2
Fata Sefer <sefer.fata@gmail.com>
2022-10-05 00:04:57 +0200

    feat: create adminSlidesProps type and specify correct types

--- src/pages/admin/slides.tsx
+++ src/pages/admin/slides.tsx
@@ -1,9 +1,33 @@
+import Head from 'next/head';
+import { SlidesProps } from '..';
+import { AppLayout } from '@/layouts/AppLayout';
 import { axios } from '@/lib/axios';
 
-const Slides = ({ slides }) => {
-  console.log(slides);
+type adminSlidesProps = SlidesProps & {
+  id: number;
+  order: number;
+  created_at: string;
+  updated_at: string;
+};
 
-  return <div>Slides</div>;
+const Slides = ({ slides }: { slides: adminSlidesProps[] }): JSX.Element => {
+  return (
+    <AppLayout
+      header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Slides</h2>}>
+      <Head>
+        <title>Countryside Kashmir - Slides</title>
+      </Head>
+      <div className="py-12">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div className="p-6 bg-white border-b border-gray-200 w-full h-96">
+              {JSON.stringify(slides)}
+            </div>
+          </div>
+        </div>
+      </div>
+    </AppLayout>
+  );
 };
 
 export const getServerSideProps = async ({
