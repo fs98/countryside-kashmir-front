14b87e671b10a34d4ac8a1f81b93e54514c38e63
Fata Sefer <sefer.fata@gmail.com>
2022-10-05 00:01:33 +0200

    feat: fetch slides from api

--- src/pages/admin/slides.tsx
+++ src/pages/admin/slides.tsx
@@ -1,3 +1,35 @@
-const Slides = () => <div>Slides</div>;
+import { axios } from '@/lib/axios';
+
+const Slides = ({ slides }) => {
+  console.log(slides);
+
+  return <div>Slides</div>;
+};
+
+export const getServerSideProps = async ({
+  req: {
+    headers: { cookie, host },
+  },
+}) => {
+  const slides = await axios
+    .get('/api/slides', {
+      headers: {
+        Cookie: cookie,
+        Referer: host,
+      },
+    })
+    .then(res => {
+      return res.data.data;
+    })
+    .catch(error => {
+      if (error.response.status !== 409) throw error;
+    });
+
+  return {
+    props: {
+      slides,
+    },
+  };
+};
 
 export default Slides;
