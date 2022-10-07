2a40aeab0538d510dc36bbaee4fd424a22ecfd75
Fata Sefer <sefer.fata@gmail.com>
2022-10-07 23:29:29 +0200

    feat: replace default app logo with countryside kashmir logo

--- src/layouts/Navigation.tsx
+++ src/layouts/Navigation.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { FC, useState } from 'react';
-import { ApplicationLogo } from '@/components/ApplicationLogo/ApplicationLogo';
 import { Dropdown } from '@/components/Dropdown/Dropdown';
 import { NavLink } from '@/components/NavLink/NavLink';
 import {
@@ -75,7 +74,11 @@ export const Navigation: FC<NavigationProps> = ({ user }) => {
             <div className="flex-shrink-0 flex items-center">
               <Link href="/admin/dashboard">
                 <a>
-                  <ApplicationLogo className="block h-10 w-auto fill-current text-gray-600" />
+                  <img
+                    src="https://countrysidekashmir.com/img/inline-logo.png"
+                    alt=""
+                    className="block h-6 w-auto fill-current text-gray-600"
+                  />
                 </a>
               </Link>
             </div>
