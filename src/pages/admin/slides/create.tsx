c7f00c31a2dfb02c00777c8d1e185ed36e3546f8
Fata Sefer <sefer.fata@gmail.com>
2022-10-09 23:26:50 +0200

    feat: render form fields from fieldsData file

--- src/pages/admin/slides/create.tsx
+++ src/pages/admin/slides/create.tsx
@@ -1,9 +1,11 @@
 import Head from 'next/head';
 import { Button, FormControl, FormHelperText, Input, InputLabel } from '@mui/material';
 import { useForm } from 'react-hook-form';
+import { fromPairs } from 'lodash';
+import { slideFormFields } from './fieldsData';
 import { AppLayout } from '@/layouts/AppLayout';
 
-type FormData = {
+export type FormData = {
   image: File;
   imageAlt: string;
   order: number;
@@ -33,69 +35,30 @@ const Slides = (): JSX.Element => {
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div className="p-6 bg-white border-b border-gray-200 grid gap-5">
               <form onSubmit={onSubmit}>
-                <FormControl color="warning" fullWidth>
-                  <InputLabel htmlFor="image">Image</InputLabel>
+                {slideFormFields.map(({ id, label, type, rules }) => {
+                  const errorHelperText =
+                    errors?.[id]?.type && rules.find(err => err.name === errors[id].type);
 
-                  <Input {...register('image', { required: true })} id="image" type="file" />
+                  return (
+                    <FormControl key={id} fullWidth={true} color="warning" sx={{ marginBottom: 5 }}>
+                      <InputLabel htmlFor={id} shrink>
+                        {label}
+                      </InputLabel>
 
-                  {errors.image?.type === 'required' && (
-                    <FormHelperText error id="component-error-text">
-                      Image is required.
-                    </FormHelperText>
-                  )}
-                </FormControl>
+                      <Input
+                        {...register(id, fromPairs(rules.map(rule => [rule.name, rule.value])))}
+                        id={id}
+                        type={type}
+                      />
 
-                <FormControl color="warning" fullWidth>
-                  <InputLabel htmlFor="image-alt">Image Alt</InputLabel>
-
-                  <Input {...register('imageAlt', { required: true, max: 64 })} id="image-alt" />
-
-                  {errors.imageAlt?.type === 'required' && (
-                    <FormHelperText error id="component-error-text">
-                      Image Alt is required.
-                    </FormHelperText>
-                  )}
-                  {errors.imageAlt?.type === 'max' && (
-                    <FormHelperText error id="component-error-text">
-                      Image Alt must be shorter than 64 characters.
-                    </FormHelperText>
-                  )}
-                </FormControl>
-
-                <FormControl color="warning" fullWidth>
-                  <InputLabel htmlFor="order">Order</InputLabel>
-
-                  <Input {...register('order', { required: true })} id="order" type="number" />
-
-                  {errors.order?.type === 'required' && (
-                    <FormHelperText error id="component-error-text">
-                      Order is required.
-                    </FormHelperText>
-                  )}
-                </FormControl>
-
-                <FormControl color="warning" fullWidth>
-                  <InputLabel htmlFor="title">Title</InputLabel>
-
-                  <Input {...register('title', { max: 64 })} id="title" />
-
-                  {errors.title?.type === 'max' && (
-                    <FormHelperText error id="component-error-text">
-                      Title must be shorter than 64 characters.
-                    </FormHelperText>
-                  )}
-                </FormControl>
-                <FormControl fullWidth>
-                  <InputLabel htmlFor="subtitle">Subtitle</InputLabel>
-
-                  <Input {...register('subtitle', { max: 32 })} id="subtitle" />
-
-                  {errors.title?.type === 'max' && (
-                    <FormHelperText error id="component-error-text">
-                      Subtitle must be shorter than 32 characters.
-                    </FormHelperText>
-                  )}
-                </FormControl>
+                      {errorHelperText?.text && (
+                        <FormHelperText error id="component-error-text">
+                          {errorHelperText.text}
+                        </FormHelperText>
+                      )}
+                    </FormControl>
+                  );
+                })}
                 <Button variant="outlined" color="warning" type="submit" sx={{ marginTop: 4 }}>
                   Submit
                 </Button>
