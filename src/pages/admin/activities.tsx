353e64f98290500e22a08d9fd74cc2e9bb055370
Fata Sefer <sefer.fata@gmail.com>
2022-10-09 23:59:07 +0200

    style: remove fixed container height

--- src/pages/admin/activities.tsx
+++ src/pages/admin/activities.tsx
@@ -13,7 +13,7 @@ const Activities = ({ activities }) => (
     <div className="py-12">
       <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-          <div className="p-6 bg-white border-b border-gray-200 w-full h-96">
+          <div className="p-6 bg-white border-b border-gray-200 w-full">
             {JSON.stringify(activities)}
           </div>
         </div>
--- src/pages/admin/destinations.tsx
+++ src/pages/admin/destinations.tsx
@@ -15,7 +15,7 @@ const Destinations = ({ destinations }) => (
     <div className="py-12">
       <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-          <div className="p-6 bg-white border-b border-gray-200 w-full h-96">
+          <div className="p-6 bg-white border-b border-gray-200 w-full">
             {JSON.stringify(destinations)}
           </div>
         </div>
