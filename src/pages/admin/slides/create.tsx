84ade19633b73a9eefdf87fff6edeeaeab0474cc
Fata Sefer <sefer.fata@gmail.com>
2023-02-06 23:02:05 +0100

    feat: handle submitting slide data

--- src/pages/admin/slides/create.tsx
+++ src/pages/admin/slides/create.tsx
@@ -2,13 +2,15 @@ import Head from 'next/head';
 import { Button, FormControl, FormHelperText, Input, InputLabel } from '@mui/material';
 import { useForm } from 'react-hook-form';
 import { fromPairs } from 'lodash';
+import { useState } from 'react';
 import { slideFormFields } from './fieldsData';
 import { AppLayout } from '@/layouts/AppLayout';
+import { axios } from '@/lib/axios';
 
 export type FormData = {
   image: File;
   imageAlt: string;
-  order: number;
+  order: string;
   title: string;
   subtitle: string;
 };
@@ -20,8 +22,50 @@ const Slides = (): JSX.Element => {
     formState: { errors },
   } = useForm<FormData>();
 
-  const onSubmit = handleSubmit(data => {
-    console.log(data);
+  const onSubmit = handleSubmit(({ image, imageAlt, order, title, subtitle }) => {
+    const formData = new FormData();
+    formData.append('image', image[0]);
+    formData.append('image_alt', imageAlt);
+    formData.append('order', order);
+    formData.append('title', title);
+    formData.append('subtitle', subtitle);
+
+    axios
+      .post('/api/slides', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then(res =>
+        setMessage({
+          title: res.data.message,
+          type: 'success',
+        }),
+      )
+      .catch(error => {
+        if (error.response.status === 500) {
+          setMessage({
+            title: error.response.data.message,
+            type: 'error',
+          });
+        }
+
+        if (error.response.status === 422) {
+          setMessage({
+            title: error.response.data.message,
+            type: 'error',
+          });
+        }
+
+        setTimeout(() => {
+          setMessage(undefined);
+        }, 3000);
+      });
+  });
+
+  const [message, setMessage] = useState<{ title: string; type: 'success' | 'error' }>({
+    title: '',
+    type: undefined,
   });
 
   return (
@@ -34,6 +78,7 @@ const Slides = (): JSX.Element => {
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div className="p-6 bg-white border-b border-gray-200 grid gap-5">
+              <div>{message.title}</div>
               <form onSubmit={onSubmit}>
                 {slideFormFields.map(({ id, label, type, rules }) => {
                   const errorHelperText =
