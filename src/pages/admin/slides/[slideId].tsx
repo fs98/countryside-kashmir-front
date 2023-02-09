532e2f291e31984588ca4409b4ae72ee5357f411
Fata Sefer <sefer.fata@gmail.com>
2023-02-09 21:32:13 +0100

    refactor: remove unused button

--- src/pages/admin/slides/[slideId].tsx
+++ src/pages/admin/slides/[slideId].tsx
@@ -1,6 +1,4 @@
 import Head from 'next/head';
-import AddIcon from '@mui/icons-material/Add';
-import Link from 'next/link';
 import { AppLayout } from '@/layouts/AppLayout';
 import { axios } from '@/lib/axios';
 
@@ -16,14 +14,6 @@ const Slide = (slide): JSX.Element => {
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div className="p-6 pb-0">
-              <Link href="slides/create">
-                <a className="btn outline p-2 rounded-sm outline-blue-500 text-blue-500 hover:outline-blue-700 hover:text-blue-700">
-                  <AddIcon />
-                  New Slide
-                </a>
-              </Link>
-            </div>
             <div className="p-6 bg-white border-b border-gray-200 w-full grid grid-cols-3 gap-4">
               Test
             </div>
