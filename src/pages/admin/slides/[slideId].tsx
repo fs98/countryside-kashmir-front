ea01894ce94fccb8e633d7e5f80b196d0243e0ed
Fata Sefer <sefer.fata@gmail.com>
2023-02-11 12:18:12 +0100

    feat: handle updating a slide

--- src/pages/admin/slides/[slideId].tsx
+++ src/pages/admin/slides/[slideId].tsx
@@ -1,9 +1,98 @@
 import Head from 'next/head';
-import { AppLayout } from '@/layouts/AppLayout';
+import { useForm } from 'react-hook-form';
+import { Button, FormControl, FormHelperText, Input, InputLabel } from '@mui/material';
+import { useState } from 'react';
+import { fromPairs } from 'lodash';
+import Router from 'next/router';
+import { slideFormFields } from './editFieldsData';
 import { axios } from '@/lib/axios';
+import { AppLayout } from '@/layouts/AppLayout';
 
-const Slide = (slide): JSX.Element => {
-  console.log(slide);
+export type FormData = {
+  image: File;
+  imageAlt: string;
+  order: string;
+  title: string;
+  subtitle: string;
+};
+
+const Slide = ({ slide }): JSX.Element => {
+  const inputProps = [
+    {
+      id: 'imageAlt',
+      attributes: {
+        defaultValue: slide.image_alt,
+      },
+    },
+    {
+      id: 'order',
+      attributes: {
+        defaultValue: slide.order,
+      },
+    },
+    {
+      id: 'title',
+      attributes: {
+        defaultValue: slide.title,
+      },
+    },
+    {
+      id: 'subtitle',
+      attributes: {
+        defaultValue: slide.subtitle,
+      },
+    },
+  ];
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit = handleSubmit(({ image, imageAlt, order, title, subtitle }) => {
+    const formData = new FormData();
+
+    formData.append('image_alt', imageAlt);
+    formData.append('order', order);
+    formData.append('title', title);
+    formData.append('subtitle', subtitle);
+
+    if (image[0]) {
+      formData.append('image', image[0]);
+    }
+
+    axios
+      .post(`/api/slides/${slide.id}?_method=PUT`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then(() => {
+        Router.push('/admin/slides');
+      })
+      .catch(error => {
+        if (error.response.status === 500) {
+          setMessage({
+            title: error.response.data.message,
+            type: 'error',
+          });
+        }
+
+        if (error.response.status === 422) {
+          setMessage({
+            title: error.response.data.message,
+            type: 'error',
+          });
+        }
+
+        setTimeout(() => {
+          setMessage(undefined);
+        }, 3000);
+      });
+  });
+
+  const [message, setMessage] = useState<{ title: string; type: 'success' | 'error' }>();
 
   return (
     <AppLayout
@@ -14,8 +103,38 @@ const Slide = (slide): JSX.Element => {
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div className="p-6 bg-white border-b border-gray-200 w-full grid grid-cols-3 gap-4">
-              Test
+            <div className="p-6 bg-white border-b border-gray-200 grid gap-5">
+              {message && <div>{message.title}</div>}
+              <form onSubmit={onSubmit}>
+                {slideFormFields.map(({ id, label, type, rules }) => {
+                  const errorHelperText =
+                    errors?.[id]?.type && rules.find(err => err.name === errors[id].type);
+
+                  return (
+                    <FormControl key={id} fullWidth={true} color="warning" sx={{ marginBottom: 5 }}>
+                      <InputLabel htmlFor={id} shrink>
+                        {label}
+                      </InputLabel>
+
+                      <Input
+                        {...register(id, fromPairs(rules.map(rule => [rule.name, rule.value])))}
+                        id={id}
+                        type={type}
+                        {...inputProps.filter(inputProp => inputProp.id === id)[0]?.attributes}
+                      />
+
+                      {errorHelperText?.text && (
+                        <FormHelperText error id="component-error-text">
+                          {errorHelperText.text}
+                        </FormHelperText>
+                      )}
+                    </FormControl>
+                  );
+                })}
+                <Button variant="outlined" color="warning" type="submit" sx={{ marginTop: 4 }}>
+                  Submit
+                </Button>
+              </form>
             </div>
           </div>
         </div>
