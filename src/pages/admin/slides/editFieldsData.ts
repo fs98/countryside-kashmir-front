680de7fa44d5e14076a14d5c8e196f1c66513bdf
Fata Sefer <sefer.fata@gmail.com>
2023-02-11 12:17:46 +0100

    feat: define editFieldsData

---
+++ src/pages/admin/slides/editFieldsData.ts
@@ -0,0 +1,78 @@
+import { FormData } from './create';
+
+type FormFieldRulesProps = {
+  name: string;
+  value: unknown;
+  text: string;
+};
+
+type FormFieldProps = {
+  id: keyof FormData;
+  label: string;
+  type: 'text' | 'number' | 'file';
+  rules: FormFieldRulesProps[];
+};
+
+type SlideFormFieldProps = FormFieldProps[];
+
+export const slideFormFields: SlideFormFieldProps = [
+  {
+    id: 'image',
+    label: 'Image (png, jpg, jpeg)',
+    type: 'file',
+    rules: [],
+  },
+  {
+    id: 'imageAlt',
+    label: 'Image Alt',
+    type: 'text',
+    rules: [
+      {
+        name: 'required',
+        value: true,
+        text: 'Image Alt is required.',
+      },
+      {
+        name: 'max',
+        value: 64,
+        text: 'Image Alt must be shorter than 64 characters.',
+      },
+    ],
+  },
+  {
+    id: 'order',
+    label: 'Order',
+    type: 'number',
+    rules: [
+      {
+        name: 'required',
+        value: true,
+        text: 'Order is required.',
+      },
+    ],
+  },
+  {
+    id: 'title',
+    label: 'Title',
+    type: 'text',
+    rules: [
+      {
+        name: 'max',
+        value: 64,
+        text: 'Title must be shorter than 64 characters.',
+      },
+    ],
+  },
+  {
+    id: 'subtitle',
+    label: 'Subtitle',
+    type: 'text',
+    rules: [
+      {
+        name: 'max',
+        value: 32,
+        text: 'Title must be shorter than 32 characters.',
+      },
+    ],
+  },
+];
