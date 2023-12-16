34c60381ae5af5be61b38eb755104cab8fcac881
Fata Sefer <sefer.fata@gmail.com>
2023-12-16 16:43:23 +0100

    feat: display destinations as cards and handle deleting

--- src/pages/admin/destinations.tsx
+++ src/pages/admin/destinations.tsx
@@ -1,28 +1,73 @@
+import { useState } from 'react';
+import AddIcon from '@mui/icons-material/Add';
 import Head from 'next/head';
+import Link from 'next/link';
+import { Card } from '@/components/Card/Card';
 import { AppLayout } from '@/layouts/AppLayout';
 import { axios } from '@/lib/axios';
+import { Destination } from '@/types/destination';
 
-const Destinations = ({ destinations }) => (
-  <AppLayout
-    header={
-      <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-        Dashboard - Destinations
-      </h2>
-    }>
-    <Head>
-      <title>Countryside Kashmir - Destinations</title>
-    </Head>
-    <div className="py-12">
-      <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-          <div className="p-6 bg-white border-b border-gray-200 w-full">
-            {JSON.stringify(destinations)}
+type DestinationsProps = {
+  destinations: Destination[];
+};
+
+const Destinations = (props: DestinationsProps) => {
+  const [destinations, setDestinations] = useState<Destination[]>(props.destinations);
+
+  const handleDelete = (destinationId: number) => {
+    axios
+      .delete(`/api/destinations/${destinationId}`)
+      .then(res => {
+        setDestinations(destinations.filter(destination => destination.id !== destinationId));
+        window.alert(res.data.message);
+      })
+      .catch(error => {
+        window.alert(error.response.data.message);
+      });
+  };
+
+  return (
+    <AppLayout
+      header={
+        <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+          Dashboard - Destinations
+        </h2>
+      }>
+      <Head>
+        <title>Countryside Kashmir - Destinations</title>
+      </Head>
+      <div className="py-12">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div className="p-6 pb-0">
+              <Link href="destinations/create">
+                <a className="btn outline p-2 rounded-sm outline-blue-500 text-blue-500 hover:outline-blue-700 hover:text-blue-700">
+                  <AddIcon />
+                  New Destination
+                </a>
+              </Link>
+            </div>
+            <div className="p-6 bg-white border-b border-gray-200 w-full grid grid-cols-3 gap-4">
+              {destinations.map(({ id, image_alt, image_url, name, keywords }) => (
+                <Card
+                  data={{
+                    image_alt,
+                    image_url,
+                    title: name,
+                    keywords,
+                  }}
+                  key={id}
+                  onDelete={() => handleDelete(id)}
+                  editUrl={`destinations/${id}/edit`}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </AppLayout>
-);
+    </AppLayout>
+  );
+};
 
 export const getServerSideProps = async ({
   req: {
