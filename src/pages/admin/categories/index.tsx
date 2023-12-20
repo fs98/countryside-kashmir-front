75569bdd7b860d85170968c93884a63f0f47f970
Fata Sefer <sefer.fata@gmail.com>
2023-12-20 23:45:33 +0100

    refactor: improve code logic and format

--- src/pages/admin/categories/index.tsx
+++ src/pages/admin/categories/index.tsx
@@ -1,21 +1,12 @@
 import { useState } from 'react';
 
-import AddIcon from '@mui/icons-material/Add';
-import {
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-} from '@mui/material';
 import { DataGrid, GridColDef, GridRowParams, GridValueGetterParams } from '@mui/x-data-grid';
-import Head from 'next/head';
-import Link from 'next/link';
 
 import moment from 'moment';
 
 import { Button } from '@/components/Button/Button';
-import { AppLayout } from '@/layouts/AppLayout';
+import { ConfirmDialog } from '@/components/ConfirmDialog/ConfirmDialog';
+import { PageLayout } from '@/layouts/PageLayout';
 import { axios } from '@/lib/axios';
 
 type CategoryProps = {
@@ -32,7 +23,7 @@ type CategoriesProps = {
 
 const Categories = (props: CategoriesProps) => {
   const [categories, setCategories] = useState<CategoryProps[]>(props.categories);
-  const [openDialog, setOpenDialog] = useState<boolean>(false);
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
   const [deleteItem, setDeleteItem] = useState<{ id: Number | null }>({ id: null });
 
   const columns: GridColDef[] = [
@@ -62,7 +53,7 @@ const Categories = (props: CategoriesProps) => {
       renderCell: ({ row }: Partial<GridRowParams>) => (
         <Button
           onClick={() => {
-            setOpenDialog(true);
+            setIsDialogOpen(true);
             setDeleteItem({
               id: row.id,
             });
@@ -76,15 +67,13 @@ const Categories = (props: CategoriesProps) => {
   const handleDelete = (categoryId: Number) => {
     axios
       .delete(`/api/categories/${categoryId}`)
-      .then(res => {
-        setCategories(prevState => prevState.filter(category => category.id !== categoryId));
-
-        window.alert(res.data.message);
+      .then(() => {
+        setCategories(prev => prev.filter(category => category.id !== categoryId));
       })
       .catch(error => {
         window.alert(error.response.data.error);
       });
-    setOpenDialog(false);
+    setIsDialogOpen(false);
   };
 
   const handleEdit = (data: GridRowParams) => {
@@ -96,8 +85,8 @@ const Categories = (props: CategoriesProps) => {
         const updatedCategory = result.data.data;
 
         // Update the state with the new data
-        setCategories(prevState => {
-          return prevState.map(category =>
+        setCategories(prev => {
+          return prev.map(category =>
             category.id === updatedCategory.id ? updatedCategory : category,
           );
         });
@@ -110,49 +99,20 @@ const Categories = (props: CategoriesProps) => {
   };
 
   return (
-    <AppLayout
-      header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Categories</h2>}>
-      <Head>
-        <title>Countryside Kashmir - Categories</title>
-      </Head>
-      <div className="py-12">
-        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
-            <DialogTitle>Delete item?</DialogTitle>
-            <DialogContent>
-              <DialogContentText>
-                Are you sure you want to delete this item? This action cannot be undone.
-              </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={() => setOpenDialog(false)}>Cancel</Button>
-              <Button onClick={() => handleDelete(deleteItem.id)} color="secondary">
-                Delete
-              </Button>
-            </DialogActions>
-          </Dialog>
-
-          <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div className="p-6 pb-0">
-              <Link href="categories/create">
-                <a className="btn outline p-2 rounded-sm outline-blue-500 text-blue-500 hover:outline-blue-700 hover:text-blue-700">
-                  <AddIcon />
-                  New Category
-                </a>
-              </Link>
-            </div>
-            <div className="p-6 bg-white border-b border-gray-200 w-full h-screen">
-              <DataGrid
-                editMode="row"
-                rows={categories}
-                columns={columns}
-                onRowEditStop={handleEdit}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </AppLayout>
+    <PageLayout resource="categories">
+      <ConfirmDialog
+        isOpen={isDialogOpen}
+        onClose={() => setIsDialogOpen(false)}
+        onConfirm={() => handleDelete(deleteItem.id)}
+      />
+      <DataGrid
+        editMode="row"
+        rows={categories}
+        columns={columns}
+        onRowEditStop={handleEdit}
+        autoHeight
+      />
+    </PageLayout>
   );
 };
 
@@ -168,9 +128,7 @@ export const getServerSideProps = async ({
         Referer: host,
       },
     })
-    .then(res => {
-      return res.data.data;
-    })
+    .then(({ data }) => data.data)
     .catch(error => {
       if (error.response.status !== 409) throw error;
     });
