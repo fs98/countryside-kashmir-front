66d90650ec3b0a7b863a850cc06395fd75dafa3b
Fata Sefer <sefer.fata@gmail.com>
2023-12-23 23:16:53 +0100

    feat: extract and pass editor js data when editing destination

--- src/blocks/DestinationsPageBlocks/DestinationForm.tsx
+++ src/blocks/DestinationsPageBlocks/DestinationForm.tsx
@@ -21,61 +21,69 @@ export const DestinationForm = ({
   editing,
   inputAttributes,
   control,
-}): JSX.Element => (
-  <form onSubmit={onSubmit}>
-    {destinationFormFields.map(({ id, label, type, rules }) => {
-      const errorHelperText =
-        errors?.[id]?.type && rules.creating.find(err => err.name === errors[id].type);
+}): JSX.Element => {
+  const editorContent = inputAttributes.find(
+    (inputProp: InputAttributesProps) => inputProp.id === 'editor',
+  )?.attributes.defaultValue;
 
-      return (
-        <FormControl key={id} fullWidth color="warning" sx={{ marginBottom: 5 }}>
-          <InputLabel htmlFor={id} shrink>
-            {label}
-          </InputLabel>
+  return (
+    <form onSubmit={onSubmit}>
+      {destinationFormFields.map(({ id, label, type, rules }) => {
+        const errorHelperText =
+          errors?.[id]?.type && rules.creating.find(err => err.name === errors[id].type);
 
-          {editing && (
-            <Input
-              {...register(id, fromPairs(rules.updating.map(rule => [rule.name, rule.value])))}
-              id={id}
-              type={type}
-              {...inputAttributes.find((inputProp: InputAttributesProps) => inputProp.id === id)
-                ?.attributes}
-            />
+        return (
+          <FormControl key={id} fullWidth color="warning" sx={{ marginBottom: 5 }}>
+            <InputLabel htmlFor={id} shrink>
+              {label}
+            </InputLabel>
+
+            {editing && (
+              <Input
+                {...register(id, fromPairs(rules.updating.map(rule => [rule.name, rule.value])))}
+                id={id}
+                type={type}
+                {...inputAttributes.find((inputProp: InputAttributesProps) => inputProp.id === id)
+                  ?.attributes}
+              />
+            )}
+
+            {!editing && (
+              <Input
+                {...register(id, fromPairs(rules.creating.map(rule => [rule.name, rule.value])))}
+                id={id}
+                type={type}
+              />
+            )}
+
+            {errorHelperText?.text && (
+              <FormHelperText error id="component-error-text">
+                {errorHelperText.text}
+              </FormHelperText>
+            )}
+          </FormControl>
+        );
+      })}
+
+      {/* Custom react-hook-form handling for editor JS */}
+      <FormControl fullWidth color="warning" sx={{ marginBottom: 5 }}>
+        <InputLabel htmlFor="editor-js" shrink>
+          Description
+        </InputLabel>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field: { onChange } }) => (
+            <CustomEditor onChange={onChange} initialData={editorContent} />
           )}
+        />
+      </FormControl>
 
-          {!editing && (
-            <Input
-              {...register(id, fromPairs(rules.creating.map(rule => [rule.name, rule.value])))}
-              id={id}
-              type={type}
-            />
-          )}
+      {errors.description && <FormHelperText error>{errors.description.message}</FormHelperText>}
 
-          {errorHelperText?.text && (
-            <FormHelperText error id="component-error-text">
-              {errorHelperText.text}
-            </FormHelperText>
-          )}
-        </FormControl>
-      );
-    })}
-
-    {/* Custom react-hook-form handling for editor JS */}
-    <FormControl fullWidth color="warning" sx={{ marginBottom: 5 }}>
-      <InputLabel htmlFor="editor-js" shrink>
-        Description
-      </InputLabel>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field: { onChange } }) => <CustomEditor onChange={onChange} />}
-      />
-    </FormControl>
-
-    {errors.description && <FormHelperText error>{errors.description.message}</FormHelperText>}
-
-    <Button variant="outlined" color="warning" type="submit" sx={{ marginTop: 4 }}>
-      Submit
-    </Button>
-  </form>
-);
+      <Button variant="outlined" color="warning" type="submit" sx={{ marginTop: 4 }}>
+        Submit
+      </Button>
+    </form>
+  );
+};
