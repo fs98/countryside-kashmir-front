16162a00601556baefb519361304108c50557520
Fata Sefer <sefer.fata@gmail.com>
2023-12-24 00:02:02 +0100

    feat: handle deleting blogs

--- src/pages/admin/blogs.tsx
+++ src/pages/admin/blogs.tsx
@@ -1,26 +1,59 @@
+import { useState } from 'react';
+
 import { Card } from '@/components/Card/Card';
+import { ConfirmDialog } from '@/components/ConfirmDialog/ConfirmDialog';
 import { PageLayout } from '@/layouts/PageLayout';
 import { axios } from '@/lib/axios';
+import { Blog } from '@/types/resources';
 
-const Blogs = ({ blogs }) => (
-  <PageLayout resource="blogs" isOverview showAddButton>
-    {blogs.map(({ id, image_alt, image_url, title, keywords }) => (
-      <Card
-        data={{
-          image_alt,
-          image_url,
-          title,
-          keywords,
-        }}
-        key={id}
-        onDelete={() => {
-          console.log('delete', id);
-        }}
-        editUrl={`activities/${id}/edit`}
+type BlogsProps = {
+  blogs: Blog[];
+};
+
+const Blogs = (props: BlogsProps) => {
+  const [blogs, setBlogs] = useState<Blog[]>(props.blogs);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [deleteItem, setDeleteItem] = useState<{ id: number | null }>({ id: null });
+
+  const handleDelete = (blogId: number) => {
+    axios
+      .delete(`/api/blogs/${blogId}`)
+      .then(() => {
+        setBlogs(blogs.filter(blog => blog.id !== blogId));
+        setIsDialogOpen(false);
+      })
+      .catch(error => {
+        window.alert(error.response.data.message);
+      });
+  };
+
+  return (
+    <PageLayout resource="blogs" isOverview showAddButton>
+      <ConfirmDialog
+        isOpen={isDialogOpen}
+        onClose={() => setIsDialogOpen(false)}
+        onConfirm={() => handleDelete(deleteItem.id)}
       />
-    ))}
-  </PageLayout>
-);
+
+      {blogs.map(({ id, image_alt, image_url, title, keywords }) => (
+        <Card
+          data={{
+            image_alt,
+            image_url,
+            title,
+            keywords,
+          }}
+          key={id}
+          onDelete={() => {
+            setIsDialogOpen(true);
+            setDeleteItem({ id });
+          }}
+          editUrl={`activities/${id}/edit`}
+        />
+      ))}
+    </PageLayout>
+  );
+};
 
 export const getServerSideProps = async ({
   req: {
